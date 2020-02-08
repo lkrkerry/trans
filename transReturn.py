41a1f61a289e3f759eb290d30647b27424ffadf1
lkrkerry <47978696+lkrkerry@users.noreply.github.com>
2020-02-08 19:22:11 +0800

    Update transReturn.py

--- transReturn.py
+++ transReturn.py
@@ -1,5 +1,7 @@
 from trans import trans as tr
 from fractions import Fraction as Fr
+
+
 def transReturn(return_obj='last', times=10,item=3.1415926,mode=1):
     trans = tr(times, item)
     transr = trans[::-1]
@@ -10,10 +12,13 @@ def transReturn(return_obj='last', times=10,item=3.1415926,mode=1):
             a = transr[0]
             for i in range(times-1):
                 a = Fr(1,a)
-                a+=trans[i+1]
+                a += trans[i+1]
             return a
         else:
             raise ValueError('Mode should be 1 or 2')
+            
+            
+            
     elif return_obj == 'all':
         if mode == 2:
             return trans
@@ -22,13 +27,13 @@ def transReturn(return_obj='last', times=10,item=3.1415926,mode=1):
             for i in range(times-1):
                 for j in range(i+1):
                     b = Fr(1,b)
-                    b+=trans[j+1]
+                    b += trans[j+1]
                     print(b)
-            return
+            return None
         else:
             raise ValueError("return_obj should be 'last' or 'all'" )
 
 
 if __name__ == "__main__":
-    a =transReturn()
+    a = transReturn()
     print(a)
